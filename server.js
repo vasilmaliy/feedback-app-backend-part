849d170a939ea79b49337dab1cfd580b37d93a5a
Maliy Vasyl <vasil.maliy14@gmail.com>
2023-03-13 17:41:38 +0200

    change posrt to 5001

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/data.routes')(app)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
